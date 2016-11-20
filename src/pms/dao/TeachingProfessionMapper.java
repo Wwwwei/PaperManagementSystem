83a8537fa220377ad0906a786a696c818e30fed1
Wwwwei <zhaosw1995@foxmail.com>
2016-11-20 15:51:28 +0800

    工程结构调整

--- src/pms/dao/TeachingProfessionMapper.java
+++
@@ -1,9 +0,0 @@
-package pms.dao;
-
-import pms.entity.TeachingProfession;
-
-public interface TeachingProfessionMapper {
-	
-	public TeachingProfession findTeachingProfessionByName(String teachingProfession_name);
-
-}
--- src/pms/service/TeachingProfessionService.java
+++
@@ -1,8 +0,0 @@
-package pms.service;
-
-import pms.entity.TeachingProfession;
-
-public interface TeachingProfessionService {
-	public TeachingProfession getTeachingProfession(String teachingProfession_name);
-
-}
