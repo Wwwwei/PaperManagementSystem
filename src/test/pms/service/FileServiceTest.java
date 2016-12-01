6c06bbb29a7941cda3c41d9ea96ba72e9cee88bc
Wwwwei <zhaosw1995@foxmail.com>
2016-12-01 11:29:48 +0800

    test

---
+++ src/test/pms/service/FileServiceTest.java
@@ -0,0 +1,37 @@
+package pms.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pms.entity.File;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class) // 表示继承了SpringJUnit4ClassRunner类
+@ContextConfiguration(locations = {"classpath:config/spring_mybatis_config.xml"})
+public class FileServiceTest {
+    @Resource
+    FileService fileService;
+
+    @Test
+    public void testCreateFile() throws Exception {
+        File file = new File();
+        file.setFile_type(2);
+        file.setFile_url("test");
+        file.setFile_paperproxy_id(2);
+        System.out.println("\n" + fileService.createFile(file));
+    }
+
+    @Test
+    public void testFindFileByPaperproxyId() throws Exception {
+        List<File> files = fileService.findFileByPaperproxyId(1);
+        for (File file : files) {
+            System.out.println("\n" + file.getFile_type());
+        }
+    }
+}
