258f744a6aac927ebe7b1d110f2983f467724e7f
Wwwwei <zhaosw1995@foxmail.com>
2016-12-01 11:09:00 +0800

    File表实体

--- src/main/java/pms/entity/File.java
+++ src/main/java/pms/entity/File.java
@@ -5,8 +5,8 @@ public class File {
     private int file_type; // 文件类型 1:作者信息页;2:论文全页;3:论文/期刊封面;4:论文/期刊封底;5:论文检索证明(国内类型1、2、3、4，国外类型2、5)
     private String file_url;// 文件路径
     private int file_isValid;//文件是否有效(预留字段)
-    private int paperproxy_id;//论文代理id
-    private int paper_id;// 论文id
+    private int file_paperproxy_id;//论文代理id
+    private int file_paper_id;// 论文id
 
     public int getFile_id() {
         return file_id;
@@ -32,14 +32,6 @@ public class File {
         this.file_url = file_url;
     }
 
-    public int getPaper_id() {
-        return paper_id;
-    }
-
-    public void setPaper_id(int paper_id) {
-        this.paper_id = paper_id;
-    }
-
     public int getFile_isValid() {
         return file_isValid;
     }
@@ -48,12 +40,19 @@ public class File {
         this.file_isValid = file_isValid;
     }
 
-    public int getPaperproxy_id() {
-        return paperproxy_id;
+    public int getFile_paperproxy_id() {
+        return file_paperproxy_id;
     }
 
-    public void setPaperproxy_id(int paperproxy_id) {
-        this.paperproxy_id = paperproxy_id;
+    public void setFile_paperproxy_id(int file_paperproxy_id) {
+        this.file_paperproxy_id = file_paperproxy_id;
     }
 
+    public int getFile_paper_id() {
+        return file_paper_id;
+    }
+
+    public void setFile_paper_id(int file_paper_id) {
+        this.file_paper_id = file_paper_id;
+    }
 }
