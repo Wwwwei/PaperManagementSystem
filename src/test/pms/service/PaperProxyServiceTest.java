453284770f75922667a04bbcfaf2c9a58791865f
Wwwwei <zhaosw1995@foxmail.com>
2016-12-07 12:08:14 +0800

    测试

--- src/test/pms/service/PaperProxyServiceTest.java
+++ src/test/pms/service/PaperProxyServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pms.entity.Journals_Conference;
+import pms.entity.Paper;
 
 import javax.annotation.Resource;
 
@@ -18,5 +20,18 @@ public class PaperProxyServiceTest {
     @Test
     public void testFindPaperProxyById() throws Exception {
         System.out.println("\n" + paperProxyService.findPaperProxyById(1).getPaper_name());
+    }
+
+    @Test
+    public void testCreatePaperProxy() throws Exception {
+        Paper paper=new Paper();
+        paper.setPaper_name("1206test02");
+        paper.setPaper_journals_conference_isZjut100(1);
+        paper.setPaper_journals_conference_isOther(0);
+        Journals_Conference journals_Conference=new Journals_Conference();
+        journals_Conference.setJournals_conference_id(1);
+        paper.setPaper_journals_conference_ESI(journals_Conference);
+        paper.setPaper_journals_conference_OTHER(journals_Conference);
+        paperProxyService.createPaperProxy(paper);
     }
 }
