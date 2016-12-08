8bb13bffe900697f56ef44b9cb280464d320706a
Wwwwei <zhaosw1995@foxmail.com>
2016-12-08 15:07:57 +0800

    论文发表方式枚举类增加初始化方法

--- src/main/java/pms/common/enums/PaperIssueEnum.java
+++ src/main/java/pms/common/enums/PaperIssueEnum.java
@@ -1,5 +1,7 @@
 package pms.common.enums;
 
+import java.util.Arrays;
+
 /**
  * 论文发表方式枚举类
  * Created by wei on 16/12/8.
@@ -22,5 +24,10 @@ public enum PaperIssueEnum {
     private PaperIssueEnum(Integer type, String description) {
         this.type = type;
         this.description = description;
+    }
+    public static PaperIssueEnum getInstance(Integer type) {
+        return Arrays.asList(values()).stream().filter(item -> type != null && item.getType().equals(type))
+                .findFirst()
+                .orElse(null);
     }
 }
