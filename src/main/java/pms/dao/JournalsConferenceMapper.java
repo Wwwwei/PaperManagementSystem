e4152c5dcf413f53dd3a15d2c2fc4fcd85c7aeb9
Wwwwei <zhaosw1995@foxmail.com>
2016-12-24 22:26:47 +0800

    删除无用代码

--- src/main/java/pms/dao/JournalsConferenceMapper.java
+++ src/main/java/pms/dao/JournalsConferenceMapper.java
@@ -6,35 +6,34 @@ import java.util.Map;
 import pms.entity.JournalsConference;
 
 public interface JournalsConferenceMapper {
-	/**
-	 * 查询所有会议或者期刊实体
-	 * @return
+    /**
+     * 查询所有会议或者期刊实体
+     *
+     * @return
      */
-//	public List<JournalsConference> findJournals_Conference();
-	public List<JournalsConference> findAll();
-	/**
-	 * 根据教师flag查询会议或者期刊集合
-	 * 
-	 * @param flag
-	 * @return
-	 */
-	//public List<JournalsConference> findJournals_ConferenceByFlag(Object flag);
-	public List<JournalsConference> findByFlag(Integer flag);
-	/**
-	 * 根据id查询会议或者期刊实体
-	 * 
-	 * @param id
-	 * @return
-	 */
-//	public JournalsConference findById(Integer journals_conference_id);
-	public JournalsConference selectById(Integer journals_conference_id);
+    public List<JournalsConference> findAll();
 
-	/**
-	 * 根据id和影响因子时间查询会议或者期刊实体
-	 * 
-	 * @param params
-	 * @return
-	 */
-//	public JournalsConference findByIdAndYear(Map<String, Object> params);
-	public JournalsConference selectByIdAndYear(Map<String, Object> params);
+    /**
+     * 根据教师flag查询会议或者期刊集合
+     *
+     * @param flag
+     * @return
+     */
+    public List<JournalsConference> findByFlag(Integer flag);
+
+    /**
+     * 根据id查询会议或者期刊实体
+     *
+     * @param journals_conference_id
+     * @return
+     */
+    public JournalsConference selectById(Integer journals_conference_id);
+
+    /**
+     * 根据id和影响因子时间查询会议或者期刊实体
+     *
+     * @param params
+     * @return
+     */
+    public JournalsConference selectByIdAndYear(Map<String, Object> params);
 }
--- src/main/java/pms/service/JournalsConferenceService.java
+++ src/main/java/pms/service/JournalsConferenceService.java
@@ -6,28 +6,28 @@ import pms.entity.JournalsConference;
 
 public interface JournalsConferenceService {
 
-	/**
-	 * 查询所有的期刊论文等级
-	 *
-	 * @return
-	 */
-	//public List<JournalsConference> findJournals_Conference();
-	public List<JournalsConference> findAllJournals_Conference();
-	/**
-	 * 根据教师flag查询会议或者期刊集合
-	 * 
-	 * @param flag
-	 * @return
-	 */
-	public List<JournalsConference> findJournals_ConferenceByFlag(Integer flag);
+    /**
+     * 查询所有的期刊论文等级
+     *
+     * @return
+     */
+    public List<JournalsConference> findAllJournals_Conference();
 
-	/**
-	 * 根据id和影响因子时间查询期刊论文实体
-	 * 
-	 * @param id
-	 * @param year
-	 * @return
-	 */
-	public JournalsConference findJournals_ConferenceByIdAndYear(Integer id, Integer year);
+    /**
+     * 根据教师flag查询会议或者期刊集合
+     *
+     * @param flag
+     * @return
+     */
+    public List<JournalsConference> findJournals_ConferenceByFlag(Integer flag);
+
+    /**
+     * 根据id和影响因子时间查询期刊论文实体
+     *
+     * @param id
+     * @param year
+     * @return
+     */
+    public JournalsConference findJournals_ConferenceByIdAndYear(Integer id, Integer year);
 
 }
